7e0fe22ff0d91faddff36dcb7fe482d73c38c1f9
abhinav3105 <abhinav0531@gmail.com>
2025-05-10 01:08:07 +0530

    stripe added

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -3,6 +3,8 @@ import { createContext, useEffect, useState } from "react";
 import { dummyCourses } from "../assets/assets";
 import { useNavigate } from "react-router-dom";
 import humanizeDuration from "humanize-duration";
+import { useAuth, useUser } from "@clerk/clerk-react";
+
 
 export const AppContext = createContext()
 
@@ -11,6 +13,8 @@ export const AppContextProvider = (props) => {
 
     const currency = import.meta.env.VITE_CURRENCY
     const navigate = useNavigate()
+    const { getToken } = useAuth()
+    const { user } = useUser()
     const [allCourses, setAllCourses] = useState([])
     const [isEducator,setIsEducator] = useState(true)
     const [enrolledCourses, setEnrolledCourses] = useState([])
@@ -82,6 +86,16 @@ export const AppContextProvider = (props) => {
         fetchAllCourses()
         fetchUserEnrolledCourses()
     }, [])
+
+    const logToken = async ()=>{
+        console.log(await getToken());
+    }
+
+    useEffect(() => {
+        if (user) {
+            logToken()
+        }
+    }, [user])
 
     const value = {
         currency  , allCourses, navigate,calculateRating,
---
+++ server/routes/educatorRoutes.js
@@ -0,0 +1,27 @@
+import express from 'express'
+import { protectEducator } from '../middlewares/authMiddleware.js';
+import upload from '../configs/multer.js';
+import { addCourse, educatorDashboardData, getEducatorCourses, getEnrolledStudentsData, updateRoleToEducator } from '../controllers/educatorController.js';
+
+
+const educatorRouter = express.Router()
+
+// Add Educator Role 
+educatorRouter.get('/update-role', updateRoleToEducator)
+
+// Add Courses 
+educatorRouter.post('/add-course', upload.single('image'), protectEducator, addCourse)
+
+
+// Get Educator Courses 
+educatorRouter.get('/courses', protectEducator, getEducatorCourses)
+
+// Get Educator Dashboard Data
+educatorRouter.get('/dashboard', protectEducator, educatorDashboardData)
+
+// Get Educator Students Data
+educatorRouter.get('/enrolled-students', protectEducator, getEnrolledStudentsData)
+
+
+
+export default educatorRouter;
