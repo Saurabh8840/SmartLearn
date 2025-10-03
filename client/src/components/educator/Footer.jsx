b767dcdef66d6ff9f5f5cdcb7134b4ab118322ca
Saurabh Tripathi <saurabhofficial8840@gmail.com>
2025-10-03 18:33:26 +0530

    few changes in footer

--- client/src/components/educator/Footer.jsx
+++ client/src/components/educator/Footer.jsx
@@ -8,7 +8,7 @@ const Footer = () => {
         <img className='hidden md:block w-20' src={assets.logo} alt="logo" />
         <div className='hidden md:block h-7 w-px bg-gray-500/60'></div>
         <p className="py-4 text-center text-xs md:text-sm text-gray-500">
-          Copyright 2025 © GreatStack. All Right Reserved.
+          Copyright 2025 © . All Right Reserved.
         </p>
       </div>
       <div className='flex items-center gap-3 max-md:mt-4'>
@@ -16,7 +16,7 @@ const Footer = () => {
           <img src={assets.facebook_icon} alt="facebook_icon" />
         </a>
         <a href="#">
-          <img src={assets.twitter_icon} alt="twitter_icon" />
+          <img src={assets.twitter_logo} alt="twitter_icon" />
         </a>
         <a href="#">
           <img src={assets.instagram_icon} alt="instagram_icon" />
